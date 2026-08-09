2e6c31fc95ede4f45d7a4dcb0bfa0ecd773e8bc7
György Császár <gyoergy.csaszar@elpro.com>
2026-08-09 10:48:21 +0200

    some changes to init files

--- tests/local.spec.ts
+++ tests/local.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('navigate to local server', async ({ page }) => {
-  await page.goto('http://127.0.0.1:55551/');
-
-  await expect(page).toHaveURL('http://127.0.0.1:55551/');
+  await page.goto('/');
   await expect(page.getByRole('heading')).toHaveText('Welcome to nginx!');
 });
